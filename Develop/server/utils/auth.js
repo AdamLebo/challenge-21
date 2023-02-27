6ee3058544dc3a5c36b503605327e02493faa882
AdamLebo <alieberman10@aol.com>
2023-02-27 09:32:00 -0500

    updated server side auth.js

--- Develop/server/utils/auth.js
+++ Develop/server/utils/auth.js
@@ -5,7 +5,12 @@ const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
-  // function for our authenticated routes
+    // function for our authenticated routes
+  signToken: function ({ username, email, _id }) {
+  const payload = { username, email, _id };
+  return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+},
+
   authMiddleware: function ({ req }) {
     // allows token to be sent via  req.query or headers
     let token = req.body.token || req.query.token || req.headers.authorization;
@@ -16,7 +21,7 @@ module.exports = {
     }
 
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      return req;
     }
 
     // verify token and get user data out of it
@@ -28,12 +33,6 @@ module.exports = {
       return res.status(400).json({ message: 'invalid token!' });
     }
 
-    // send to next endpoint
-    next();
-  },
-  signToken: function ({ username, email, _id }) {
-    const payload = { username, email, _id };
-
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+    return req;
   },
 };
