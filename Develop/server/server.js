cd8727fc29711669340aa7f39af434df10145c41
AdamLebo <alieberman10@aol.com>
2023-02-27 09:38:33 -0500

    added apollo and middleware to server side server.js

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -8,6 +8,11 @@ const { authMiddleware } = require("./utils/auth");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const { ApolloServer } = require('apollo-server-express');
+const authMiddleware = require('./utils/auth');
+const path = require('path');
+const db = require('./config/connection');
+const routes = require('./routes');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
